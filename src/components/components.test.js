76bd311061d18c9d629451025b1366ab1fcc472f
franclobo <fjbl2788@gmail.com>
2022-07-15 22:09:40 -0500

    Run test

--- src/components/components.test.js
+++ src/components/components.test.js
@@ -1,29 +1,16 @@
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
-import configureStore from '../Redux/configureStore';
+import configureStore from '../redux/configureStore';
 import Homepage from './Homepage';
-import Details from './Details';
 
 describe('Components Test', () => {
-  test('HomePage Test', () => {
+  test('Homepage Test', () => {
     const store = configureStore;
     const component = renderer.create(
       <Provider store={store}>
         <MemoryRouter>
           <Homepage />
-        </MemoryRouter>
-      </Provider>,
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-  test('Details Test', () => {
-    const store = configureStore;
-    const component = renderer.create(
-      <Provider store={store}>
-        <MemoryRouter>
-          <Details />
         </MemoryRouter>
       </Provider>,
     );
